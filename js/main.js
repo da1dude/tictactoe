0ec7c48840647e06dc4e9406152420ebee381c16
Paul K <pkerschieter@gmail.com>
2023-11-20 21:25:35 -0600

    added main game logic and tweaked messages and css

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 //////////////////////////////////
 
 const colors = {
-    0: 'white',
+    null: 'white',
     1: 'purple',
     // if we want to use a negative number for a key
     // we can use a string
@@ -27,7 +27,7 @@ const winningCombos = [
 //////////////////////////////////
 // things that we want to constantly check, refer to, and change.
 // these will allow us to properly render changes at the right time.
-let board // an array of 7 nested arrays
+let board 
 let turn // will be a value of 1 or -1 (1 || -1)
 let winner // null || 1 || -1 || 'T'
 
@@ -50,9 +50,8 @@ function init() {
     // set values for our state variables
     turn = 1
     winner = null
-
     board = [null, null, null, null, null, null, null, null, null]
-    
+
     // CALL THE RENDER FUNCTION ONCE THE RENDER FUNCTION IS BUILT
     render()
 }
@@ -61,18 +60,33 @@ init()
 
 // function - renderBoard - render the game board
 function renderBoard() {
-
-
+    board.forEach(function(boxVal, index) {
+        const boxEl = document.getElementById(index);
+        boxEl.style.backgroundColor = colors[boxVal];
+        console.log('squareEl value', boxEl);
+        //console.log('boxVal value', boxVal);
+        console.log('board array', board)
+    });
 }
 
-// render controls -> changes the visibility of the play again button
-function renderControls() {
 
-}
+// // render controls -> changes the visibility of the play again button
+// function renderControls() {
+
+// }
 
 // render message -> display whose turn it is
 function renderMessage() {
-
+    // message a tie
+    if (winner === 'T') {
+        message.innerText = "Tie!"
+    // message a winner
+    } else if (winner) {
+        message.innerHTML = `<span style="color: ${colors[winner]}">${colors[winner].toUpperCase()}</span> WINS!`;
+    // or the current turn
+    } else {
+        message.innerHTML = `<span style="color: ${colors[turn]}">${colors[turn].toUpperCase()}</span>'s Turn`;
+    }
 }
 
 // render -> call all of our render based functions at once
@@ -81,13 +95,22 @@ function render() {
     renderBoard()
     // call renderMessage
     renderMessage()
-    // call renderControls
-    renderControls()
 }
 
 // handleDrop -> this will be the main gameplay function, finds the marker that was clicked on, and drops to the bottommost position allowed
-function handleDrop(event) {
- 
+function boxClicked(event) {
+    // obtain index of box
+    const index = event.target.id;  
+    console.log("index clicked, should update array with 1/-1", index)
+    if (
+        //if index taken or winner.. dont have winner working...
+        board[index] || winner
+    ) return;
+    board[index] = turn; //fought with this... forgot to update array with the turn value
+    console.log("board array status after each turn", board)
+    turn *= -1;
+    //winner = getWinner();
+    render();
 }
 
 
@@ -98,6 +121,6 @@ function handleDrop(event) {
 //////////////////////////////////
 // what events will happen, what they should be attached to, and what functions they call
 // click on a marker (to make a move)
-document.getElementById('markers').addEventListener('click', boxClicked)
+document.getElementById('board').addEventListener('click', boxClicked)
 // click play again (initialize an empty board) (reset all variables)
 playAgainButton.addEventListener('click', init)
